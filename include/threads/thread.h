e119bc3341d48edb006592ccec6d48ce5aaddd5c
Ubuntu <ubuntu@ip-172-31-46-132.ap-northeast-2.compute.internal>
2024-12-02 07:36:24 +0000

    feat: init commit + errors fixed

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -11,8 +11,6 @@
 #include "vm/vm.h"
 #endif
 
-#define USERPROG  // Project 2: 충돌 방지
-
 /* States in a thread's life cycle. */
 enum thread_status {
     THREAD_RUNNING, /* Running thread. */
@@ -24,7 +22,7 @@ enum thread_status {
 /* Thread identifier type.
    You can redefine this to whatever type you like. */
 typedef int tid_t;
-#define TID_ERROR ((tid_t)-1) /* Error value for tid_t. */
+#define TID_ERROR ((tid_t) - 1) /* Error value for tid_t. */
 
 /* Thread priorities. */
 #define PRI_MIN     0  /* Lowest priority. */
@@ -144,7 +142,14 @@ typedef struct thread {
 #ifdef VM
     /* Table for whole virtual memory owned by thread. */
     struct supplemental_page_table spt;
+
+    /** Project 3: Anonymous Page - stack용 포인터 생성*/
+    void *stack_bottom;
+    void *stack_pointer;
 #endif
+
+    /** Project 4: Filesys - File System */
+    struct dir *cwd; // Current Working Directory
 
     /* Owned by thread.c. */
     struct intr_frame tf; /* Information for switching */
