e15794cbc5f3cddd6a79e99cdd8bcb6b25dc7d40
20hyukim <20hyukim@naver.com>
2024-12-03 06:47:11 +0000

    feat: vm_file_init & file_backed_initializer edited in file.c

--- vm/file.c
+++ vm/file.c
@@ -19,15 +19,29 @@ static const struct page_operations file_ops = {
 /* The initializer of file vm */
 void
 vm_file_init (void) {
+	//anon과 다르게 디스크에 메모리를 할당 받을 필요가 없다. file이라는 HDD 가 있으니까.
 }
 
 /* Initialize the file backed page */
 bool
 file_backed_initializer (struct page *page, enum vm_type type, void *kva) {
 	/* Set up the handler */
+	/* pseudo : 
+	 * file_backed와 관련된 operation을 설정
+	 * UNINIT에서 FILE_BACKED라는 상태를 명시.
+	 */
 	page->operations = &file_ops;
 
 	struct file_page *file_page = &page->file;
+
+	/* do_mmap: aux에 file 정보들을 저장해 두었던 것을 file_page에 옮겨 담아준다.
+	aux - UNINIT 페이지일 때, 쓰던 것 */
+	struct aux *aux = (struct aux *)page->uninit.aux;
+	file_page->file = aux->file;
+	file_page->offset = aux->offset;
+	file_page->page_read_bytes = aux->page_read_bytes;
+
+	return true;
 }
 
 /* Swap in the page by read contents from the file. */
